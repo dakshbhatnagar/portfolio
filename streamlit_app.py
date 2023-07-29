ced476a88a91910cf7d4fd20dd5b308c2390a09b
Daksh Bhatnagar <bhatnagar91@gmail.com>
2023-07-29 15:55:02 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import random
 """
 # Welcome to Streamlit!
 
@@ -12,3 +13,31 @@ In the meantime, below is an example of what you can do with just a few lines of
 
 
 st.title('Hello World')
+wins = 0
+losses = 0
+ties = 0
+
+while True:
+    playerChoice = input("Enter rock, paper, or scissors (or q to quit): ")
+
+    if playerChoice.lower() == "q":
+        break
+
+    choices = ["rock", "paper", "scissors"]
+    computerChoice = random.choice(choices)
+
+    st.markdown("The computer chose:", computerChoice)
+
+    if computerChoice == playerChoice:
+        st.markdown("Draw!")
+        ties += 1
+    elif (playerChoice == "paper" and computerChoice == "rock") or \
+            (playerChoice == "rock" and computerChoice == "scissors") or \
+            (playerChoice == "scissors" and computerChoice == "paper"):
+        st.markdown("Won!")
+        wins += 1
+    else:
+        st.markdown("Lost!")
+        losses += 1
+
+st.markdown("Wins:", wins, "Losses:", losses, "Ties:", ties)
