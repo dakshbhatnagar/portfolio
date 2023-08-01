f94233559b4dcaf697d7884b9dc4c7cfe7370702
Daksh Bhatnagar <bhatnagar91@gmail.com>
2023-08-01 17:44:49 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ st.markdown("<h1 style='text-align: center; color: black;'>Daksh Bhatnagar</h1>"
 st.markdown("<h2 style='text-align: center; color: light grey;'>Data Analyst</h2>", unsafe_allow_html=True)
 st.sidebar.title("Welcome")
 st.sidebar.info(
-    "This app contains all the projects which I was intrigued by and that I have done. "
+    "This app contains all the projects which I was intrigued by and that I have done.")
     ""
 """
 
