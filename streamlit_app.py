5459955bd96866b1836bb28c09de6695af26d503
Daksh Bhatnagar <bhatnagar91@gmail.com>
2023-08-01 17:02:17 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,4 @@
 import streamlit as st
-import random
 st.title('Daksh Bhatnagar's Portfolio')
 """
 👋 Hey there, fellow Earthlings! 🌍 Daksh Bhatnagar reporting for data duty! 📈 As a Data Analyst from Delhi, India, I have embarked on a quest to conquer the mysteries of data one byte at a time! 🕵️‍♂️
