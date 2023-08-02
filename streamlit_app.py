adcdf5bfd79aed40aceaf8bf4a199b5523863696
Daksh Bhatnagar <bhatnagar91@gmail.com>
2023-08-03 00:17:43 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,7 @@ st.markdown(hide_default_format, unsafe_allow_html=True)
 with st.sidebar:
     add_radio = st.markdown("<h1 style='text-align: center; color: black; font-size: 55px;'>Welcome</h1>", 
                 unsafe_allow_html=True)
-    sidebar_info = st.markdown("<h2 style='text-align: center; color: black; font-size: 15px;'>This app showcases my passion for data and tech through the collection of the work I've done. Hope you enjoy your stay!</h1>", 
+    sidebar_info = st.markdown("<h2 style='text-align: center; color: black; font-size: 18px;'>This app showcases my passion for data and tech through the collection of the work I've done. Hope you enjoy your stay!</h1>", 
                 unsafe_allow_html=True)
 
 def intro():
@@ -21,24 +21,21 @@ def intro():
     st.write("""
     📈 As a Data Analyst from Delhi, India, I enjoy unravelling the mysteries of data one byte at a time! 🕵️‍♂️
 
-    ✅ With the help of Google Sheets and Looker, I can help you look deeper into your business and make informed decisions. Feel free to check out my [Tableau](https://public.tableau.com/app/profile/daksh.bhatnagar#!/) profile.
+    ✅ With the help of Google Sheets & Looker, I can help you look deeper into your business & make informed decisions. Feel free to check out my [Tableau](https://public.tableau.com/app/profile/daksh.bhatnagar#!/) profile.
 
-    🔥 I am always on the quest for business problems that can be solved with the help of data and some automation. Problem Solving is fun!.
+    🔥 I'm always on the quest for business problems that can be solved with the help of data and some automation. Problem Solving is fun!.
 
-    💡 They say I'm a Kaggle Notebooks Expert but don't trust them. See for yourself on this : [link](https://www.kaggle.com/bhatnagardaksh/code)
+    💡 They say I'm a Kaggle Notebooks Expert but don't trust them. See for yourself [here](https://www.kaggle.com/bhatnagardaksh/code).
 
-    🤖 I am also developing CNNs for [Zone Classification](https://www.kaggle.com/code/bhatnagardaksh/zone-classification-transfer-learning) for my unpaid side gig.
-
-    🍻 If you have got a data problem then I'm more than willing to help 🎉 Whether it's discovering hidden patterns or making sense of the data chaos, I'm your data ally! 
-
-""")
+    🤖 I'm also developing CNNs for [Zone Classification](https://www.kaggle.com/code/bhatnagardaksh/zone-classification-transfer-learning) for my unpaid side gig. 
+    """)
 
 def python_proj():
     st.write(
         """
-        ### 📁 Python Projects
+        ##### 📁 Python Projects
 
-        Here is a glimpse of projects that I have done using Python:
+        Here is a glimpse of projects that I've done using Python:
 
         1. **Customer Churn Prediction** 📊
             - Dive into the world of customer behavior analysis! Using ANN, we find out if the credit card customer is going to be churned or not. Click [here](https://github.com/dakshbhatnagar/python_files/blob/main/JupyterNotebooks/customer-churn-prediction-anns%20(1).ipynb) 
@@ -50,29 +47,31 @@ def python_proj():
             - Understanding Sentiment of others can be tough. Let's make it a breeze. Click [here](https://www.kaggle.com/code/bhatnagardaksh/youtube-comments-analysis-updated) to find out how
 
         **And Many More!** 🚀 
+
+        Check out the relevant Github Repository [here](https://github.com/dakshbhatnagar/python_files)
         
         """
     )
 def JS_page():
     st.write("""
-    ### 📁 JavaScript Mini Projects
+    ##### 📁 JavaScript Mini Projects
 
 1. **Random Number Guesser** 🎲
-    Are you a psychic when it comes to guessing numbers? Test your intuition with this fun and interactive Number Guesser game [here](https://github.com/dakshbhatnagar/JS_files/blob/main/index.js) 🤞
+    Are you a psychic when it comes to guessing numbers? Test your intuition with this fun & interactive Number Guesser game [here](https://github.com/dakshbhatnagar/JS_files/blob/main/index.js) 🤞
 
 2. **Job Scraper** 📝
-   The Job Scraper built using fetch API and regex concepts pulls the data from job portal and store it in a CSV. Take a look [here](https://github.com/dakshbhatnagar/JS_files/blob/main/scraper.js) 💼🔍
+   The Job Scraper built using fetch API and regex concepts pulls the data from job portal & store it in a CSV. Take a peek [here](https://github.com/dakshbhatnagar/JS_files/blob/main/scraper.js) 💼🔍
 
 3. **Snake, Water, and Gun Game** 🐍🌊🔫
-   Challenge the computer to a game of Snake, Water, and Gun! Here's how it works:
+   Challenge the computer to a [game](https://github.com/dakshbhatnagar/JS_files/blob/main/snake_water_gun.js) of Snake, Water, and Gun! Here's how it works:
 
-   a. 🐍 Snake beats 🔫 Gun: Snakes are sneaky! They'll take down guns any day.
+   a. Snake beats Gun: Snakes are sneaky! They'll take down guns any day.
    
-   b. 🔫 Gun beats 🌊 Water: Guns can shoot down even the most formidable waves.
+   b. Gun beats Water: Guns can shoot down even the most formidable waves.
    
-   c. 🌊 Water beats 🐍 Snake: Water will drown the slithering snakes with ease.
+   c. Water beats Snake: Water will drown the slithering snakes with ease.
 
-    Check out the [project](https://github.com/dakshbhatnagar/JS_files/blob/main/snake_water_gun.js)! 🎲""")
+    """)
 
 def contact_me():
     st.markdown("Contact :")
@@ -82,7 +81,7 @@ def contact_me():
 
     🚀 If you want to connect or just say Hi, connect with me on [LinkedIn](https://www.linkedin.com/in/dakshb/) 
             
-    Got questions, or suggestions? I'm all ears! Feel free to reach out to me at bhatnagar91@gmail.com, and I promise to respond faster than ChatGPT! ⚡️!        
+    ✉️ Got questions, or suggestions? I'm all ears! Let me hear it at bhatnagar91@gmail.com !        
         """)
 
 page_names_to_funcs = {
