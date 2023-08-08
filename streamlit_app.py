c4f53ef91b9fedc43e75bb3e78bbd4b13331fa20
Daksh Bhatnagar <bhatnagar91@gmail.com>
2023-08-08 17:17:44 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import numpy as np
 import yfinance as yf
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -24,6 +25,11 @@ symbol = st.text_input('Enter a Symbol Here e.g GOOGL or AAPL', '')
 data = yf.Ticker(symbol)
 data_hist = data.history(period="max")
 df = data_hist[["Open", "High", "Low", "Close", "Volume"]]
+
+@st.cache_data
+def convert_df(df):
+    # IMPORTANT: Cache the conversion to prevent computation on every rerun
+    return df.to_csv().encode('utf-8')
 
 def predict():
     if symbol:
@@ -52,46 +58,75 @@ def predict():
         fig, ax = plt.subplots()
         ax.set_ylabel('Price')
         ax.set_xlabel('Time')
-        
-        ax.legend(['Actual Price', 'Predicted Price'])
         plt.gca().spines['top'].set_visible(False)
         plt.gca().spines['right'].set_visible(False);
         ax.plot(arr)
+        plt.gca().legend(('Close','Predictions'))
         st.pyplot(fig)
         col1, col2 = st.columns(2)
         with col1:
             st.markdown("<h5 style='text-align: center; color: black; font-size: 20px;'>Actual Price and Predictions</h1>", 
                     unsafe_allow_html=True)
             st.dataframe(arr)
+            csv = convert_df(arr)
+            st.download_button(label="Download data as CSV",data=csv,file_name='predictions.csv',mime='text/csv')
+             
+            MSE = np.square(np.subtract(test.Close,test.Forecast)).mean()
+            rmse = round(np.sqrt(MSE),2)
+            Col1, Col2, Col3 = st.columns(3)
+
+            with Col1:
+                st.write(' ')
+
+            with Col2:
+                st.metric(label='RMSE', value=rmse)
+
+            with Col3:
+                st.write(' ')
+            
+            st.info("*RMSE (Root Mean Square Error) is a measure used to find how accurate a prediction model is by calculating the average difference between predicted and actual values in a dataset.")
+
         with col2:
             st.write("About :")
             st.write(data.info['longBusinessSummary'])
             st.write(f"Industry : {data.info['industry']}")
             st.write(f"CEO : {data.info['companyOfficers'][0]['name']}")
     else:
-        st.markdown('You did not enter a symbol.')
+        st.markdown("<h5 style='text-align: center; color: black; font-size: 20px;'>Please enter a symbol above!</h5>", 
+                    unsafe_allow_html=True)
+    
 
 def explore():
     
-    st.markdown(f"<h5 style='text-align: center; color: black; font-size: 15px;'>The model was trained on {data_hist.shape[0]} days worth of data</h1>", unsafe_allow_html=True)
+    st.markdown(f"<h5 style='text-align: center; color: black; font-size: 15px;'>Collected {data_hist.shape[0]} days worth of data</h1>", unsafe_allow_html=True)
     with st.expander("See Original Data"):
         st.dataframe(df)
-    
+        csv = convert_df(df)
+        st.download_button(label="Download data as CSV",data=csv,file_name='Raw Data.csv',mime='csv')
+
     col1, col2 = st.columns(2)
     with col1:
         st.markdown("<h5 style='text-align: center; color: red; font-size: 20px;'>Institutional Holders</h5>", 
                     unsafe_allow_html=True)
         st.dataframe(data.institutional_holders)
+        csv = convert_df(data.institutional_holders)
+        st.download_button(label="Download data as CSV",data=csv,file_name='institutional_holders.csv',mime='text/csv')
         
         st.markdown("<h5 style='text-align: center; color: red; font-size: 20px;'>Cash Flow</h5>", 
                     unsafe_allow_html=True)
         st.dataframe(data.cashflow)
+        csv = convert_df(data.cashflow)
+        st.download_button(label="Download data as CSV",data=csv,file_name='cashflow.csv',mime='text/csv')
 
     with col2:
         st.markdown("<h5 style='text-align: center; color: red; font-size: 20px;'>Balance Sheet</h5>", unsafe_allow_html=True)
         st.dataframe(data.balancesheet)
+        csv = convert_df(data.balancesheet)
+        st.download_button(label="Download data as CSV",data=csv,file_name='balancesheet.csv',mime='text/csv')
         st.markdown("<h5 style='text-align: center; color: red; font-size: 20px;'>Earning Dates</h5>", unsafe_allow_html=True)
         st.dataframe(data.earnings_dates)
+        csv = convert_df(data.earnings_dates)
+        st.download_button(label="Download data as CSV",data=csv,file_name='earnings_dates.csv',mime='text/csv')
 
 page_names_to_funcs = {
     "Predict": predict,
@@ -99,9 +134,10 @@ page_names_to_funcs = {
 }
 
 with st.sidebar:
-    st.markdown("<h1 style='text-align: center; color: black; font-size: 30px;'>Stock Predictions using ARIMA</h1>", 
+    st.markdown("<h1 style='text-align: center; color: black; font-size: 30px;'>Stock Predictions using yfinance</h1>", 
                 unsafe_allow_html=True)
-    st.markdown("<h6 style='text-align: center; color: black; font-size: 15px;'>The ARIMA model which we are using here is used as a forecasting tool to predict how something will act in the future based on past performance</h6>", 
+    url = "https://machinelearningmastery.com/arima-for-time-series-forecasting-with-python/"
+    st.markdown("<p style='text-align: center; color: black; font-size: 12px;'>The prediction model used is ARIMA which is a statistical model used to predict future values in time series data by combining past data and patterns.</p>", 
                 unsafe_allow_html=True)
     demo_name = st.selectbox('Predict Or Explore', page_names_to_funcs.keys())
     
