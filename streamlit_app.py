e24383893dd9121b1159a74d416547e7f21636c1
Daksh Bhatnagar <bhatnagar91@gmail.com>
2023-11-29 21:47:41 +0530

    GenAI powered now

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,17 +2,31 @@ import streamlit as st
 import numpy as np
 import yfinance as yf
 import pandas as pd
+import os
 import plotly.express as px
+from dotenv import load_dotenv
 import statsmodels.tsa.stattools as ts
 import statsmodels.api as sm
 from datetime import date, timedelta
 import plotly.graph_objects as go
+import google.generativeai as palm
+
+load_dotenv()
+
+key = os.environ.get("key")
+
+palm.configure(api_key=key)
+
+models = [m for m in palm.list_models() if 'generateText' in m.supported_generation_methods]
+llm = models[0].name
+
 hide_default_format = """
        <style>
        #MainMenu {visibility: hidden; }
        footer {visibility: hidden;}
        </style>
        """
+
 
 st.set_page_config(page_title='Stock Predictions', layout="wide", initial_sidebar_state="auto")
 
@@ -22,11 +36,6 @@ symbol = st.text_input('Enter a Symbol Here e.g GOOGL or AAPL', '')
 data = yf.Ticker(symbol)
 data_hist = data.history(period="max")
 df = data_hist[["Open", "High", "Low", "Close", "Volume"]]
-
-@st.cache_data
-def convert_df(df):
-    # IMPORTANT: Cache the conversion to prevent computation on every rerun
-    return df.to_csv().encode('utf-8')
 
 def predict():
     if symbol:
@@ -65,8 +74,6 @@ def predict():
             st.markdown("<h5 style='text-align: center; color: black; font-size: 20px;'>Actual Price and Predictions</h1>", 
                     unsafe_allow_html=True)
             st.dataframe(arr)
-            csv = convert_df(arr)
-            st.download_button(label="Download data as CSV",data=csv,file_name='predictions.csv',mime='text/csv')
              
             MSE = np.square(np.subtract(test.Close,test.Forecast)).mean()
             rmse = round(np.sqrt(MSE),2)
@@ -84,30 +91,36 @@ def predict():
             st.info("*RMSE (Root Mean Square Error) is a measure used to find how accurate a prediction model is by calculating the average difference between predicted and actual values in a dataset.")
 
         with col2:
-            st.write("About :")
-            st.write(data.info['longBusinessSummary'])
-            st.write(f"Industry : {data.info['industry']}")
-            st.write(f"CEO : {data.info['companyOfficers'][0]['name']}")
+            prompt = f"You are a learned being and help others with their queries. Tell the user a little bit of history about this {symbol} and dont make it too long and keep it only 1 para long"
+
+            completion = palm.generate_text(model=llm,prompt=prompt,temperature=0,max_output_tokens=800)
+
+            about = completion.result 
+            st.write("About :", about)
     else:
        st.markdown("<h5 style='text-align: center; color: black; font-size: 30px;'>Welcome to Stock Prediction App!</h5>", 
                     unsafe_allow_html=True)
-       text = "We're excited to have you on board. In this project, we leverage the powerful capabilities of the Yahoo Finance library and an ARIMA model to provide real-time predictions like never before."    
+       
+       prompt = """
+        You are a learned being and help others with their queries
+
+        Write an introduction about an app that predicts the historical stock prices using a statistical model called ARIMA. Dont complicate
+        as the audience might not be technically sound and dont talk about us having a website. Welcome the user and wish them well.
+        """
+
+       completion = palm.generate_text(model=llm,prompt=prompt,temperature=0.2,max_output_tokens=800)
+
+       text = completion.result 
        st.markdown(f"<p style='text-align: center; color: black; font-size: 17px;'>{text}</p>", 
                     unsafe_allow_html=True)
-       text = "The approach involves training the prediction model on the fly as soon as you input your data. This means that you can receive accurate forecasts tailored to your specific needs, all in real-time. Say goodbye to pre-trained models and hello to dynamic predictions that adapt to your requirements."
-       st.markdown(f"<p style='text-align: center; color: black; font-size: 17px;'>{text}</p>", 
-                    unsafe_allow_html=True)
-       text = "The app provides a user-friendly interface for you to interact with the prediction engine seamlessly. Get ready to experience the future of financial forecasting! In the explore section, you can also get to know about the institutional holders, the company's cash flow, balance sheet etc"
-       st.markdown(f"<p style='text-align: center; color: black; font-size: 17px;'>{text}</p>", 
-                    unsafe_allow_html=True)
+       
+       
 def explore():
     
     st.markdown(f"<h5 style='text-align: left; color: black; font-size: 12px;'>Collected {data_hist.shape[0]} days worth of data</h1>", unsafe_allow_html=True)
     
     with st.expander("See Original Data"):
         st.dataframe(df)
-        csv = convert_df(df)
-        st.download_button(label="Download data as CSV",data=csv,file_name='Raw Data.csv',mime='csv')
     
     df.index = df.index.date
     days_to_subtract = 365
@@ -131,25 +144,17 @@ def explore():
         st.markdown("<h5 style='text-align: center; color: red; font-size: 20px;'>Institutional Holders</h5>", 
                     unsafe_allow_html=True)
         st.dataframe(data.institutional_holders)
-        csv = convert_df(data.institutional_holders)
-        st.download_button(label="Download data as CSV",data=csv,file_name='institutional_holders.csv',mime='text/csv')
         
         st.markdown("<h5 style='text-align: center; color: red; font-size: 20px;'>Cash Flow</h5>", 
                     unsafe_allow_html=True)
         st.dataframe(data.cashflow)
-        csv = convert_df(data.cashflow)
-        st.download_button(label="Download data as CSV",data=csv,file_name='cashflow.csv',mime='text/csv')
+
 
     with col2:
         st.markdown("<h5 style='text-align: center; color: red; font-size: 20px;'>Balance Sheet</h5>", unsafe_allow_html=True)
         st.dataframe(data.balancesheet)
-        csv = convert_df(data.balancesheet)
-        st.download_button(label="Download data as CSV",data=csv,file_name='balancesheet.csv',mime='text/csv')
         st.markdown("<h5 style='text-align: center; color: red; font-size: 20px;'>Earning Dates</h5>", unsafe_allow_html=True)
         st.dataframe(data.earnings_dates)
-        csv = convert_df(data.earnings_dates)
-        st.download_button(label="Download data as CSV",data=csv,file_name='earnings_dates.csv',mime='text/csv')
-    
     
 page_names_to_funcs = {
     "Predict": predict,
@@ -169,7 +174,7 @@ with st.sidebar:
     with col3:
         st.write(' ')
     
-    st.markdown("<h1 style='text-align: center; color: black; font-size: 25px;'>Stock Predictions using yfinance</h1>", 
+    st.markdown("<h1 style='text-align: center; color: black; font-size: 25px;'>Stock Prediction App</h1>", 
                 unsafe_allow_html=True)
     st.markdown("<p style='text-align: center; color: black; font-size: 10px;'>The prediction model used is ARIMA, a statistical model used to predict future values by combining past data and patterns.</p>", 
                 unsafe_allow_html=True)
@@ -182,7 +187,7 @@ with st.sidebar:
 
     🚀 Connect with me on [LinkedIn](https://www.linkedin.com/in/dakshb/) 
             
-    ✉️ Shoot me an email at bhatnagar91@gmail.com !        
+    ✉️ Shoot me an [email](bhatnagar91@gmail.com)!        
         """)
 
 page_names_to_funcs[demo_name]()
