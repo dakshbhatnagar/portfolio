43bf08bc8711cc8411ec8efa759a1eae166b3074
Daksh Bhatnagar <bhatnagar91@gmail.com>
2023-12-03 12:02:54 +0530

    Commented Code

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,14 @@
+# Import necessary libraries
 import streamlit as st
 import numpy as np
 import yfinance as yf
 import pandas as pd
-import os
 import plotly.express as px
 import statsmodels.tsa.stattools as ts
 import statsmodels.api as sm
 from datetime import date, timedelta
 import plotly.graph_objects as go
-
+# Hide default Streamlit format for cleaner appearance
 hide_default_format = """
        <style>
        #MainMenu {visibility: hidden; }
@@ -20,14 +20,18 @@ hide_default_format = """
 st.set_page_config(page_title='Stock Predictions', layout="wide", initial_sidebar_state="auto")
 
 st.markdown(hide_default_format, unsafe_allow_html=True)
+# Get user input for stock symbol
 
 symbol = st.text_input('Enter a Symbol Here e.g GOOGL or AAPL', '')
 data = yf.Ticker(symbol)
 data_hist = data.history(period="max")
 df = data_hist[["Open", "High", "Low", "Close", "Volume"]]
 
+# Function to predict and display stock metrics and predictions
 def predict():
+    
     if symbol:
+        # Display real-time stock metrics
         col1, col2, col3, col4= st.columns(4)
         with col1:
             open_val = np.around(data.history(period='today').Open.values,2)
@@ -43,7 +47,8 @@ def predict():
             st.metric(label='Close',value=close_val)
         st.markdown(f"<h5 style='text-align: center; color: black; font-size: 20px;'>Predicting {symbol} Close prices</h1>", 
                     unsafe_allow_html=True)
-           
+        
+        # Train ARIMA model   
         train = df[df.index.year<2021]
         test = df[df.index.year>=2021]
         st.write(f"Model trained on {len(train)} days worth of data")
@@ -52,15 +57,31 @@ def predict():
         test = test[test.columns[:4]]
         model = sm.tsa.arima.ARIMA(endog=train['Close'], exog=train[exogenous_features], order=(1, 1, 1))
         model_fit = model.fit()
-        
+
+        # Generate forecast
         forecast = [model_fit.forecast(exog=test[exogenous_features].iloc[i]).values[0] for i in range(len(test))]
         test['Forecast'] = forecast
+        
+        # Plot prediction chart
         arr = test[['Close','Forecast']][-30:]
         fig = px.line(arr,color_discrete_sequence=["#0474BA", "#F17720"])
         st.plotly_chart(fig, theme="streamlit",  title="Prediction Chart", use_container_width=True)
+        
+        # Plot scatter plot for actual vs predicted close prices
+        fig = px.scatter(arr, x='Close', y='Forecast', color_discrete_sequence=["#0474BA", "#F17720"],
+                        trendline="ols",  # Ordinary Least Squares regression line,
+                        trendline_color_override="#F17720",
+                        labels={'Close': 'Actual Close', 'Forecast': 'Predicted Close'},
+                        title='Actual vs Predicted Close Prices',
+                        opacity=0.7,
+                        size_max=15)  # Adjust the marker size
+
+        st.plotly_chart(fig, theme="streamlit",  title="Actuals and Predictions Scattterplot", use_container_width=True)
+        
+        # Display metrics and information
         col1, col2 = st.columns(2)
         with col1:
-            st.markdown("<h5 style='text-align: center; color: black; font-size: 20px;'>Actual Price and Predictions</h1>", 
+            st.markdown("<h5 style='text-align: center; color: black; font-size: 20px;'>Actual Close and Predictions</h1>", 
                     unsafe_allow_html=True)
             st.dataframe(arr)
              
@@ -80,6 +101,7 @@ def predict():
             st.info("*RMSE (Root Mean Square Error) is a measure used to find how accurate a prediction model is by calculating the average difference between predicted and actual values in a dataset.")
 
         with col2:
+            # Display information about the stock
             about = data.get_info()['longBusinessSummary']
             
             st.write("About :\n\n", about)
@@ -100,9 +122,9 @@ def predict():
        st.markdown(f"<p style='text-align: center; color: black; font-size: 17px;'>{text}</p>", 
                     unsafe_allow_html=True)
        
-       
+# Function to explore stock data and display various information       
 def explore():
-    
+    # Display collected data and candlestick chart
     st.markdown(f"<h5 style='text-align: left; color: black; font-size: 12px;'>Collected {data_hist.shape[0]} days worth of data</h1>", unsafe_allow_html=True)
     
     with st.expander("See Original Data"):
@@ -110,6 +132,7 @@ def explore():
             st.dataframe(df)
     
     if symbol:
+        # Display candlestick chart
         df.index = df.index.date
         days_to_subtract = 365
         end = date.today()
@@ -126,7 +149,7 @@ def explore():
         fig = go.Figure(data=[trace], layout=layout)
         st.plotly_chart(fig)
 
-
+        # Display institutional holders, cash flow, balance sheet, and earning dates
         col1, col2 = st.columns(2)
         with col1:
             st.markdown("<h5 style='text-align: center; color: red; font-size: 20px;'>Institutional Holders</h5>", 
@@ -143,12 +166,12 @@ def explore():
             st.dataframe(data.balancesheet)
             st.markdown("<h5 style='text-align: center; color: red; font-size: 20px;'>Earning Dates</h5>", unsafe_allow_html=True)
             st.dataframe(data.earnings_dates)
-    
+# Dictionary mapping page names to corresponding functions    
 page_names_to_funcs = {
     "Predict": predict,
     "Explore": explore
 }
-
+# Streamlit sidebar
 with st.sidebar:
     logo ='https://upload.wikimedia.org/wikipedia/commons/thumb/8/8f/Yahoo%21_Finance_logo_2021.png/1200px-Yahoo%21_Finance_logo_2021.png?20220131010522'
     
@@ -166,6 +189,7 @@ with st.sidebar:
                 unsafe_allow_html=True)
     st.markdown("<p style='text-align: center; color: black; font-size: 10px;'>The prediction model used is ARIMA, a statistical model used to predict future values by combining past data and patterns.</p>", 
                 unsafe_allow_html=True)
+    # Dropdown to select the page (Predict or Explore)
     demo_name = st.selectbox('Predict Or Explore', page_names_to_funcs.keys())
     
     st.markdown("Contact :")
@@ -177,5 +201,6 @@ with st.sidebar:
             
     ✉️ Shoot me an [email](bhatnagar91@gmail.com)!        
         """)
+# Call the selected function based on the dropdown selection
 
 page_names_to_funcs[demo_name]()
