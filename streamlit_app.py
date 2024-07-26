228ee07707c779a610c2f26f6833f54f4b404524
Daksh Bhatnagar <bhatnagar91@gmail.com>
2024-07-26 21:00:14 +0530

    Update App

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,96 +1,141 @@
 import streamlit as st
-st.set_page_config(page_title='Daksh Bhatnagar | Data Analyst', layout="wide")
-st.markdown("<h1 style='text-align: center; font-size: 28px; color: #ffffff;'>Daksh Bhatnagar</h1>", unsafe_allow_html=True)
-st.markdown("<h2 style='text-align: center; font-size: 25px; color: #ffffff;'>Data Analyst</h2>", unsafe_allow_html=True)
-hide_default_format = """
-       <style>
-       #MainMenu {visibility: hidden; }
-       footer {visibility: hidden;}
-       </style>
-       """
 
+# Page configuration
+st.set_page_config(
+    page_title='Daksh Bhatnagar | Data Analyst',
+    layout="wide",
+    initial_sidebar_state='expanded'
+)
+
+# Hide default Streamlit formatting
+hide_default_format = """
+<style>
+#MainMenu {visibility: hidden;}
+footer {visibility: hidden;}
+</style>
+"""
 st.markdown(hide_default_format, unsafe_allow_html=True)
 
+# Main header
+st.markdown(
+    """
+    <h1 style='text-align: center; font-size: 28px; color: #ffffff;'>Daksh Bhatnagar</h1>
+    <h2 style='text-align: center; font-size: 25px; color: #ffffff;'>Data Analyst</h2>
+    </div>
+    """,
+    unsafe_allow_html=True
+)
+
+# Sidebar
 with st.sidebar:
-    add_radio = st.markdown("<h1 style='text-align: center; color: #FCF5ED; font-size: 55px;'>Welcome</h1>", 
-                unsafe_allow_html=True)
-    sidebar_info = st.markdown("<h2 style='text-align: center; color: #FCF5ED; font-size: 16px;'>This app showcases my passion for data and tech through the collection of the work I've done. Hope you enjoy your stay!</h1>", 
-                unsafe_allow_html=True)
-
-def intro():
-    st.markdown("Introduction")
-    st.write("""
-
-    📈 As a Data Analyst from New Delhi, India, I enjoy unraveling the mysteries of data one byte at a time! 
-
-    ✅ With the help of [Google Sheets](https://docs.google.com/spreadsheets/d/14h0UCZOhi1nQx7oT7DY8SYmqp3S0Y5UssEjkGAuVgXo/edit#gid=312503756) & Looker, I can help you look deeper into your business & make informed decisions. Feel free to check out my [Tableau](https://public.tableau.com/app/profile/daksh.bhatnagar#!/) profile.
-
-    🔥 I'm always on the quest for business problems that can be solved with the help of data and automation.
-
-    💡 They say I'm a Kaggle Notebooks Master but don't trust them. See for yourself [here](https://www.kaggle.com/bhatnagardaksh/code).
-
-    🤖 I'm also developing CNNs for [Zone Classification](https://www.kaggle.com/code/bhatnagardaksh/zone-classification-transfer-learning) for my freelancing project and I am open to take on other freelance projects as well.
-
-    """)
-
-def python_proj():
-    st.write(
+    st.markdown(
         """
-        ##### 📁 Python Projects
+        <h1 style='text-align: center; color: #FCF5ED; font-size: 36px;'>Welcome</h1>
+        <h2 style='text-align: center; color: #FCF5ED; font-size: 16px;'>This app showcases my passion for data and tech through my work collection. Hope you enjoy your stay!</h2>
+        </div>
+        """,
+        unsafe_allow_html=True
+    )
 
-        Here is a glimpse of projects that I've done using Python:
+# Functions for different pages
+def intro():
+    st.markdown(
+        """
+        ## Introduction
+        **📈 As a Data Analyst from New Delhi, India, I enjoy unraveling the mysteries of data one byte at a time!**
 
-        1. **Customer Churn Prediction** 📊
-            - Dive into the world of customer behavior analysis! Using ANN, we find out if the credit card customer is going to be churned or not. Click [here](https://github.com/dakshbhatnagar/python_files/blob/main/JupyterNotebooks/customer-churn-prediction-anns%20(1).ipynb) 
+        ✅ Using [Google Sheets](https://docs.google.com/spreadsheets/d/14h0UCZOhi1nQx7oT7DY8SYmqp3S0Y5UssEjkGAuVgXo/edit#gid=312503756) & Looker, I can help you make informed business decisions. Check out my [Tableau](https://public.tableau.com/app/profile/daksh.bhatnagar#!/) profile.
 
-        2. **Stock Price Predictions** 📈
-            - Stock Price predictions are always fun to make. Using Backtesting, a statistical model, and a Deep Learning Network, we do exactly that. Check out the project [here](https://www.kaggle.com/code/bhatnagardaksh/stock-predictions-with-backtesting-arima-and-gru). I have also made an app around the same as well which you can check out on this [link](https://stockpredictions.streamlit.app/)
+        🔥 I'm always on the quest for business problems that can be solved with data and automation.
 
-        3. **Sentiment Analysis** 📝
-            - Understanding the Sentiments of others can be tough. Let's make it a breeze. Click [here](https://www.kaggle.com/code/bhatnagardaksh/youtube-comments-analysis-updated) to find out how
+        💡 I'm a Kaggle Notebooks Master. See for yourself [here](https://www.kaggle.com/bhatnagardaksh/code).
 
-        **And Many More!** 🚀 
+        🤖 Currently developing CNNs for [Zone Classification](https://www.kaggle.com/code/bhatnagardaksh/zone-classification-transfer-learning) and open to freelance projects.
+        """,
+        unsafe_allow_html=True
+    )
 
-        Check out the relevant Github Repository [here](https://github.com/dakshbhatnagar/python_files)
+def google_sheets():
+    st.markdown(
+        """
+        ### 📊 Google Sheets Experience
+        - Leveraged Google Sheets and AppScript for cost reduction and automation in business processes.
+
+        - Developed a Google Sheets pipeline by bringing in data from different worksheets for instant insights, boosting data-driven decisions by 20%.
+
+        - Automated daily reports with Google AppScript, ensuring regular updates to the management and reducing manual work which led to 15% increase in efficiency.
+
+        - Automated monthly incentive calculations (recruiters and TL) and distributions with Google Sheets, AppScript, reducing manual work by 15%.
         
+        - [Project Example](https://docs.google.com/spreadsheets/d/14h0UCZOhi1nQx7oT7DY8SYmqp3S0Y5UssEjkGAuVgXo/edit#gid=312503756)
         """
     )
-def JS_page():
-    st.write("""
-    ##### 📁 JavaScript Mini Projects
 
-1. **Random Number Guesser** 🎲
-    Are you a psychic when it comes to guessing numbers? Test your intuition with this fun & interactive Number Guesser game [here](https://github.com/dakshbhatnagar/JS_files/blob/main/index.js) 
+def python_proj():
+    st.markdown(
+        """
+        ### 📁 Python Projects
 
-2. **Job Scraper** 📝
-   The Job Scraper built using fetch API and regex concepts pulls the data from the job portal & stores it in a CSV. Take a peek [here](https://github.com/dakshbhatnagar/JS_files/blob/main/scraper.js) 
+        **1. Customer Churn Prediction** 📊 - Analyze customer behavior with ANN. [Explore here](https://github.com/dakshbhatnagar/python_files/blob/main/JupyterNotebooks/customer-churn-prediction-anns%20(1).ipynb).
 
-3. **Snake, Water, and Gun Game** 🐍🌊🔫
-   Challenge the computer to a [game](https://github.com/dakshbhatnagar/JS_files/blob/main/snake_water_gun.js) of Snake, Water, and Gun! Here's how it works:
+        **2. Stock Price Predictions** 📈 - Predict stock prices using Backtesting, ARIMA, and GRU. [Check it out](https://www.kaggle.com/code/bhatnagardaksh/stock-predictions-with-backtesting-arima-and-gru).
 
-   a. Snake beats Gun: Snakes are sneaky! They'll take down guns any day.
-   
-   b. Gun beats Water: Guns can shoot down even the most formidable waves.
-   
-   c. Water beats Snake: Water will drown the slithering snakes with ease.
+        **3. Sentiment Analysis** 📝 - Simplify sentiment analysis. [Learn more](https://www.kaggle.com/code/bhatnagardaksh/youtube-comments-analysis-updated).
 
-    """)
+        **And Many More!** 🚀
+
+        View more on [GitHub](https://github.com/dakshbhatnagar/python_files).
+        """,
+        unsafe_allow_html=True
+    )
+
+def sql_projects():
+    st.markdown(
+        """
+        ###  SQL Projects
+        🧬 Using SQL I analysed HR Data where in I looked at questions like:-
+
+        - How many people are in each job
+        - Which job pays more
+        - Here is the [Project Link](https://github.com/dakshbhatnagar/SQLProjects/tree/main/HRData)
+
+        - Advanced SQL queries for data extraction and analysis.
+
+        - 🙋‍♂️ [SQL GitHub Repository](https://github.com/dakshbhatnagar/SQLProjects)
+        """
+    )
+
+def visualization():
+    st.markdown(
+        """
+        ### 📈 BI Tool Projects
+
+        - 🧾 Trend of Digital Payments in India - [Link](https://public.tableau.com/app/profile/daksh.bhatnagar/viz/IndianDigitalPayments/Dashboard1)
+        
+        - 💉  COVID -19 Dashboard - [Link](https://public.tableau.com/app/profile/daksh.bhatnagar/viz/COVID-19Dashboard_16433078005920/Dashboard)
+        
+        - 🙋‍♂️ Check out the whole [Tableau Profile](https://public.tableau.com/app/profile/daksh.bhatnagar#!/)
+        """
+    )
 
 def contact_me():
-    st.markdown("Contact :")
-    
-    st.write(
-        """ 
+    st.markdown(
+        """
+        ## Contact
+        🚀 Connect with me on [LinkedIn](https://www.linkedin.com/in/dakshb/).
 
-    🚀 If you want to connect or just say Hi, connect with me on [LinkedIn](https://www.linkedin.com/in/dakshb/) 
-            
-    ✉️ Got questions or suggestions? I'm all ears! Let me hear it at bhatnagar91@gmail.com!        
-        """)
+        ✉️ Questions or suggestions? Email me at bhatnagar91@gmail.com!
+        """,
+        unsafe_allow_html=True
+    )
 
+# Page navigation
 page_names_to_funcs = {
     "Introduction": intro,
+    "Google Sheets": google_sheets,
     "Python": python_proj,
-    "JavaScript": JS_page,
+    "SQL": sql_projects,
+    "Visualization": visualization,
     "Contact": contact_me
 }
 
